2340bff9f86d32fe5ee82bf3d2c9528407524d85
Ernest Vilà <ernest.vila@cirvianum.cat>
2023-12-17 20:04:58 +0100

    Canviat estils del login/register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased bg-[#31304D]">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased bg-[#31304D]" style="opacity: 0.9;">
+    <div class="absolute inset-0 z-[-1]"
+        style="background-image: url('{{ asset('https://img.freepik.com/vector-gratis/papel-pintado-abstracto-blanco_23-2148817745.jpg?w=1380&t=st=1702839204~exp=1702839804~hmac=273ffdee52fd1b9d2a2e52e2a17cd56f82fc818d162992712046db8cb9271e8b') }}');
+           background-size: cover;
+           background-repeat: no-repeat;
+           opacity: 0.2;">
+    </div>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 text-gray-300">
+        <div>
+            <a href="/" class="hover:text-gray-500 transition-colors duration-300">
+                <h1 class="text-3xl font-black">Perruqueria Institut Cirviànum</h1>
+            </a>
         </div>
-    </body>
+
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white text-gray-900 shadow-md overflow-hidden sm:rounded-lg">
+
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
